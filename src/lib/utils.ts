fce8eed9ad3233305961a374acd079af30ba4651
PatrickMCardoso <patrick.cardoso@estudante.iftm.edu.br>
2025-08-05 16:18:35 -0300

    chore: add eslint, prettier and shadcn setup

---
+++ src/lib/utils.ts
@@ -0,0 +1,10 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+
+  // essa classe permite que não haja conflito
+  // caso eu sete alguma className pra um botão
+  // sendo que ele já tem seu próprio tailwind
+}
