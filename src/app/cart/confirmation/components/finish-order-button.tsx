a1b4a7d95a8428792db6c97c8d5e24bc5d50645e
PatrickMCardoso <patrick.cardoso@estudante.iftm.edu.br>
2025-08-13 23:01:38 -0300

    feat: ensure stripe session is created

--- src/app/cart/confirmation/components/finish-order-button.tsx
+++ src/app/cart/confirmation/components/finish-order-button.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { loadStripe } from "@stripe/stripe-js";
 import { Loader2 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
+import { createCheckoutSession } from "@/actions/create-checkout-session";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -19,8 +21,24 @@ const FinishOrderButton = () => {
   const [successDialogIsOpen, setSuccessDialogIsOpen] = useState(false);
   const finishOrderMutation = useFinishOrder();
 
-  const handleFinishOrder = () => {
-    finishOrderMutation.mutate();
+  const handleFinishOrder = async () => {
+    if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+      throw new Error("Stripe publishable key not configured");
+    }
+    const { orderId } = await finishOrderMutation.mutateAsync();
+    const checkoutSession = await createCheckoutSession({
+      orderId,
+    });
+    const stripe = await loadStripe(
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    );
+    if (!stripe) {
+      throw new Error("Stripe not initialized");
+    }
+    await stripe.redirectToCheckout({
+      sessionId: checkoutSession.id,
+    });
+
     setSuccessDialogIsOpen(true);
   };
 
@@ -49,7 +67,7 @@ const FinishOrderButton = () => {
           <DialogTitle className="mt-4 text-2xl">Pedido Efetuado!</DialogTitle>
           <DialogDescription className="font-medium">
             Seu pedido foi efetuado com sucesso. Você pode acompanhar o status
-            na seção de “Meus Pedidos”.
+            na seção de &quot;Meus Pedidos&quot;.
           </DialogDescription>
           <DialogFooter>
             <Button className="rounded-full" size="lg">
--- src/app/cart/confirmation/page.tsx
+++ src/app/cart/confirmation/page.tsx
@@ -4,7 +4,6 @@ import { redirect } from "next/navigation";
 
 import Footer from "@/components/common/footer";
 import { Header } from "@/components/common/header";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { db } from "@/db";
 import { cartTable } from "@/db/schema";
